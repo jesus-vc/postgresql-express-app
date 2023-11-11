cb5cb3cd28fa124c9594afd779237f0177513766
Jesus Vasquez-Cipriano <jcvasq@ucdavis.edu>
2023-11-11 13:05:54 -0800

    adjusting seed file format

--- seeds/data_mn.sql
+++ seeds/data_mn.sql
@@ -34,39 +34,26 @@ PRIMARY KEY (industries_code, companies_code));
 
 
 INSERT INTO companies
-VALUES ('apple', 'Apple Computer',
-'Maker of OSX.'), ('tesla',
-'Tesla Cars',
-'Maker of Teslas'), ('pfz',
-'Pfizer',
-'Maker of Drugs'), ('ibm',
-'IBM',
-'Big blue.');
+VALUES ('apple', 'Apple Computer','Maker of OSX.'),
+('tesla','Tesla Cars','Maker of Teslas'),
+('pfz','Pfizer','Maker of Drugs'),
+('ibm','IBM','Big blue.');
 
 
 INSERT INTO invoices (comp_Code, amt, paid, paid_date)
-VALUES ('apple',
-100,
-false,
-null), ('apple', 200,
-false,
-null), ('apple',
-300,
-true,
-'2018-01-01'), ('ibm',
-400,
-false,
-null);
+VALUES ('apple', 100, false, null),
+('apple', 200, false, null),
+('apple', 300, true, '2018-01-01'),
+('ibm', 400, false, null);
 
 
 INSERT INTO industries (code, industry)
-VALUES ('acct',
-'Accounting'), ('softw',
-'Software'), ('med',
-'Medical'), ('pharm',
-'Pharmaceutical'), ('manuf',
-'Manufacturing'), ('trans',
-'Transportation');
+VALUES ('acct', 'Accounting'),
+('softw', 'Software'),
+('med', 'Medical'),
+('pharm', 'Pharmaceutical'),
+('manuf', 'Manufacturing'),
+('trans', 'Transportation');
 
 
 INSERT INTO itype (industries_code,companies_code)
@@ -75,4 +62,3 @@ VALUES ('softw','apple'),
 ('pharm','apple'),
 ('manuf','pfz'),
 ('manuf','tesla');
-
